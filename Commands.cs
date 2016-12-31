dc598c8c921c98d1cd4f23b897cdafaaa9c25aca
Albert Szilvasy <albert.szilvasy@autodesk.com>
2016-12-30 18:54:59 -0800

    Initial commit

---
+++ Commands.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: ExtensionApplication(null)]
+[assembly: CommandClass(typeof(SectionPlaneToBlock.Commands))]
+
+namespace SectionPlaneToBlock
+{
+    public class Commands
+    {
+        [CommandMethod("-SP2B")]
+        public static void Sp2B()
+        {
+            var doc = Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.MdiActiveDocument;
+            var opt = new PromptEntityOptions("Select section object");
+            opt.SetRejectMessage("Please select a section.");
+            opt.AddAllowedClass(typeof(Section), false);
+            var res = doc.Editor.GetEntity(opt);
+            if (res.Status != Autodesk.AutoCAD.EditorInput.PromptStatus.OK)
+                return;
+            using (var t = doc.TransactionManager.StartOpenCloseTransaction())
+            {
+                var section = (Section)t.GetObject(res.ObjectId, OpenMode.ForRead);
+                var settings = (SectionSettings)t.GetObject(section.Settings, OpenMode.ForWrite);
+                //customize the section using the settings object
+                settings.CurrentSectionType = SectionType.Section2d;
+                //etc.
+                t.Commit();
+            }
+            if (!SystemObjects.DynamicLinker.IsModuleLoaded("acsection.crx"))
+                SystemObjects.DynamicLinker.LoadModule("acsection.crx", false, true);
+            CliSectionPlaneToBlock.Run(res.ObjectId);
+        }
+    }
+}
---
+++ Native.cs
@@ -0,0 +1,130 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SectionPlaneToBlock
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    struct IAcApHostDialog
+    {
+        IntPtr pvtable; //vtable pointer
+        IntPtr vtable; //vtable (1 entry)
+        public IntPtr objectId;
+        public static unsafe void Init(IntPtr* this_, ShowDialog showDialog, ObjectId id)
+        {
+            //initialize vtable
+            this_[1] = Marshal.GetFunctionPointerForDelegate(showDialog);
+            //initialize vtable pointer to point to vtable
+            this_[0] = new IntPtr(&this_[1]);
+            this_[2] = id.OldIdPtr;
+        }
+        [UnmanagedFunctionPointer(CallingConvention.ThisCall)]
+        public delegate int ShowDialog(ref IAcApHostDialog this_, IntPtr parameters);
+    }
+    class NativeMethods
+    {
+        [DllImport(
+            dllName: "accore.dll",
+            CallingConvention = CallingConvention.ThisCall,
+            ExactSpelling = true,
+            CharSet = CharSet.Unicode,
+            EntryPoint = "?setHost@AcApHostDialog@@QEAAXPEAVIAcApHostDialog@@@Z"),
+        ]
+        public extern static void AcApHostDialog_setHost(IntPtr this_, IntPtr host);
+
+        [DllImport(
+            dllName: "acsection.crx",
+            CallingConvention = CallingConvention.Cdecl,
+            ExactSpelling = true,
+            CharSet = CharSet.Unicode,
+            EntryPoint = "?instance@AcApHostDialogSectionGenerateSettings@@SAPEAV1@XZ"),
+        ]
+        public extern static IntPtr AcApHostDialogSectionGenerateSettings_instance();
+
+        [DllImport(
+            dllName: "acsection.crx",
+            CallingConvention = CallingConvention.ThisCall,
+            ExactSpelling = true,
+            CharSet = CharSet.Unicode,
+            EntryPoint = "?setSectionId@AcApHostSectionGenerateSettingsDialogServices@@QEAAXAEBVAcDbObjectId@@@Z"),
+        ]
+        public extern static IntPtr AcApHostSectionGenerateSettingsDialogServices_setSectionId(IntPtr this_, ref IntPtr objectId);
+
+        [DllImport(
+            dllName: "accore.dll",
+            CallingConvention = CallingConvention.ThisCall,
+            ExactSpelling = true,
+            CharSet = CharSet.Unicode,
+            EntryPoint = "?setReturnCode@AcApHostDialogParams@@QEAAX_J@Z"),
+        ]
+        public extern static void AcApHostDialogParams_setReturnCode(IntPtr this_, IntPtr retCode);
+        [DllImport(
+            dllName: "acsection.crx",
+            CallingConvention = CallingConvention.ThisCall,
+            ExactSpelling = true,
+            CharSet = CharSet.Unicode,
+            EntryPoint = "?getDialogServices@AcApHostSectionGenerateSettingsDialogParams@@QEBAPEAVAcApHostSectionGenerateSettingsDialogServices@@XZ"),
+        ]
+        public extern static IntPtr AcApHostSectionGenerateSettingsDialogParams_getDialogServices(IntPtr this_);
+    }
+
+    class CliSectionPlaneToBlock : IDisposable
+    {
+        IAcApHostDialog m_native = new IAcApHostDialog();
+        IntPtr m_ptr;
+        GCHandle m_pin;
+        IAcApHostDialog.ShowDialog m_delegate;
+        static int showDialog(ref IAcApHostDialog this_, IntPtr parameters)
+        {
+            NativeMethods.AcApHostDialogParams_setReturnCode(parameters, new IntPtr(1));
+            var svc = NativeMethods.AcApHostSectionGenerateSettingsDialogParams_getDialogServices(parameters);
+            NativeMethods.AcApHostSectionGenerateSettingsDialogServices_setSectionId(svc, ref this_.objectId);
+            return 0;
+        }
+        unsafe CliSectionPlaneToBlock(ObjectId section)
+        {
+            m_pin = GCHandle.Alloc(m_native, GCHandleType.Pinned);
+            m_ptr = m_pin.AddrOfPinnedObject();
+            m_delegate = new IAcApHostDialog.ShowDialog(showDialog);
+            IAcApHostDialog.Init((IntPtr*)m_ptr.ToPointer(), m_delegate, section);
+        }
+        IntPtr NativePtr { get { return m_ptr; } }
+        public static void Run(ObjectId section)
+        {
+            using (var fake = new CliSectionPlaneToBlock(section))
+            {
+                //provide AcSection.crx with a fake dialog implementation. This allows us to reuse the 
+                //block generation code.
+                var dlgProxy = NativeMethods.AcApHostDialogSectionGenerateSettings_instance();
+                NativeMethods.AcApHostDialog_setHost(dlgProxy, fake.NativePtr);
+                var ed = Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.MdiActiveDocument.Editor;
+                ed.Command("_SECTIONPLANETOBLOCK");
+            }
+        }
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                m_pin.Free();
+                disposedValue = true;
+            }
+        }
+        ~CliSectionPlaneToBlock()
+        {
+            Dispose(false);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
